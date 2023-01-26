2ebd139c69fec1f9c8a664b0a03aeef3267da627
앵가리 <>
2023-01-27 05:45:19 +0900

    오류 처리

--- app.js
+++ app.js
@@ -2,21 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt')
 const session = require('express-session')
 const MySQLStore = require('express-mysql-session')(session)
 const flash = require('connect-flash')
 const passport = require('passport')
 const passportConfig = require('./passport')
 passportConfig()
-
-const template = require('./lib/template')
-const auth = require('./lib/auth')
-const db = require('./lib/db')
 const secrets = require('./secrets')
 
-const indexRouter = require('./routes/index.js');
-const authRouter = require('./routes/auth.js');
+const indexRouter = require('./routes/index.js')
+const authRouter = require('./routes/auth.js')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(session({
@@ -29,8 +24,17 @@ app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use('/', indexRouter);
-app.use('/auth', authRouter);
+app.use('/', indexRouter)
+app.use('/auth', authRouter)
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found.')
+});
+
+app.use((err, req, res, next) => {
+    console.error(error.stack)
+    res.status(500).send('Internal Server Error.')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
