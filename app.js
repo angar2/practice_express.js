d70160f665f265714ceb8a96f271ccad4599a4b3
앵가리 <>
2023-01-26 20:13:22 +0900

    페이지 로드: signup

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ const port = 3000
 const template = require('./lib/template')
 
 app.get('/', (req, res) => {
-  res.send(template.html())
+    let title = '메모장'
+    let body = template.bodyHome(title)
+    let html = template.html(body)
+    res.send(html)
+})
+
+app.get('/signup', (req, res) => {
+    let title = 'Sign up'
+    let body = template.bodySignup(title)
+    let html = template.html(body)
+    res.send(html)
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
