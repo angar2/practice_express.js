b321bce1ffbbb76b3bdb219f32ea08dc7c715337
앵가리 <>
2023-01-30 14:37:16 +0900

    게시물 리스트

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 const template = require('../lib/template')
 const auth = require('../lib/auth')
+const db = require('../lib/db')
 
-router.get('/', (req, res) => {
+router.get('*', (req, res, next) => {
+    db.query(`SELECT id, title FROM topic`, (err, results) => {
+        req.topicList = results;
+        next();
+    })
+})
+
+router.get('/', async (req, res) => {
     let title = '메모장'
+    let list = await template.list(req, res)
     let authStatus = auth.Status(req, res)
-    let body = template.bodyHome(title, authStatus)
+    let body = template.bodyHome(title, authStatus, list)
     let html = template.html(body)
     res.send(html)
 })
