b0fd9abf23a2d89aeff8bc055f2082d92fff8489
앵가리 <>
2023-01-30 19:47:45 +0900

    엑세스 권한 부여: 게시물 작성자

--- routes/topic.js
+++ routes/topic.js
@@ -27,42 +27,62 @@ router.post('/create', auth.isLoggedIn, (req, res) => {
     })
 })
 
-router.get('/update/:topicId', async (req, res) => {
+router.get('/update/:topicId', auth.isLoggedIn, async (req, res) => {
     let topicId = path.parse(req.params.topicId).base
     db.query(`SELECT * FROM topic WHERE id=?`,[topicId], async (err, results) => {
         if(err) {throw err}
-        let title = 'Update'
-        let list = await template.list(req, res)
-        let authStatus = auth.Status(req, res)
-        let body = template.bodyUpdate(title, results, authStatus, list)
-        let html = template.html(body)
-        res.send(html)
+        let isOwner = await auth.isOwner(req, res, results[0].user_id)
+        if(!isOwner) {
+            req.flash('error', 'You do not have access')
+            res.redirect(`/topic/${topicId}`)
+        } else {
+            let title = 'Update'
+            let list = await template.list(req, res)
+            let authStatus = auth.Status(req, res)
+            let body = template.bodyUpdate(title, results, authStatus, list)
+            let html = template.html(body)
+            res.send(html)
+        }
     })
 })
 
-router.post('/update', (req, res) => {
+router.post('/update', auth.isLoggedIn, async (req, res) => {
     let post = req.body
-    let topic_id = post.id
+    let topic_id = post.topic_id
     let title = post.title
     let desc = post.desc
-    db.query(`UPDATE topic SET
-        title=?, description=?
-        WHERE id=?`,
-        [title, desc, topic_id],
-        (err, result) => {
-            if(err) {throw err}
-            res.redirect(`/topic/${topic_id}`)
-    })
+    // let user_id = post.user_id
+    // let isOwner = await auth.isOwner(req, res, user_id)
+    // if(!isOwner) {
+    //     req.flash('error', 'You do not have access')
+    //     res.redirect(`/topic/${topic_id}`)
+    // } else {
+        db.query(`UPDATE topic SET
+            title=?, description=?
+            WHERE id=?`,
+            [title, desc, topic_id],
+            (err, result) => {
+                if(err) {throw err}
+                res.redirect(`/topic/${topic_id}`)
+        })
+    // }
 })
 
-router.post('/delete', (req, res) => {
-    let topic_id = req.body.id
-    db.query(`DELETE FROM topic WHERE id=?`,
-        [topic_id],
-        (err, result) => {
-            if(err) {throw err}
-            res.redirect(`/`)
-    })
+router.post('/delete', auth.isLoggedIn, async (req, res) => {
+        let user_id = req.body.user_id
+        let topic_id = req.body.topic_id
+        let isOwner = await auth.isOwner(req, res, user_id)
+        if(!isOwner) {
+            req.flash('error', 'You do not have access')
+            res.redirect(`/topic/${topic_id}`)
+        } else {
+        db.query(`DELETE FROM topic WHERE id=?`,
+            [topic_id],
+            (err, result) => {
+                if(err) {throw err}
+                res.redirect(`/`)
+        })
+    }
 })
 
 router.get('/:topicId', async (req, res) => {
@@ -71,11 +91,12 @@ router.get('/:topicId', async (req, res) => {
         if(err) {throw err}
         let user_id = results[0].user_id
         db.query(`SELECT nickname FROM user WHERE id=?`,[user_id], async (err2, nickname) => {
-            if(err) {throw err}
+            if(err2) {throw err2}
             results[0].nickname = nickname[0].nickname
             let list = await template.list(req, res)
             let authStatus = auth.Status(req, res)
-            let body = template.bodyDetail(results, authStatus, list)
+            let feedback = auth.feedback(req, res)
+            let body = template.bodyDetail(results, authStatus, list, feedback)
             let html = template.html(body)
             res.send(html)
         })
